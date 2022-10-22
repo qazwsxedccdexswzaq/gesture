834d0858d1cf31d62568711509da132d79bd7657
qazwsxedccdexswzaq <96393413+qazwsxedccdexswzaq@users.noreply.github.com>
2022-10-22 10:14:20 -1000

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,75 @@ function modelLoaded()
 {
     console.log('Model Loaded!');
 }
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1="The first prediction is " + prediction_1;
+    speak_data_2="and the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if (results[0].label == "👍")
+        {
+            document.getElementById("update_emoji").innerHTML="👍 is Good";
+        }
+        if (results[0].label == "👎")
+        {
+            document.getElementById("update_emoji").innerHTML="👎 is Bad";
+        } 
+        if (results[0].label == "✋")
+        {
+            document.getElementById("update_emoji").innerHTML="✋ is Five";
+        } 
+        if (results[0].label == "👌")
+        {
+            document.getElementById("update_emoji").innerHTML="👌 is OK";
+        } 
+        if (results[0].label == "✌️")
+        {
+            document.getElementById("update_emoji").innerHTML="✌️ is Two";
+        }  
+        if (results[1].label == "👍")
+        {
+            document.getElementById("update_emoji2").innerHTML="👍 is Good";
+        }
+        if (results[1].label == "👎")
+        {
+            document.getElementById("update_emoji2").innerHTML="👎 is Bad";
+        } 
+        if (results[1].label == "✋")
+        {
+            document.getElementById("update_emoji2").innerHTML="✋ is Five";
+        } 
+        if (results[1].label == "👌")
+        {
+            document.getElementById("update_emoji2").innerHTML="👌 is OK";
+        } 
+        if (results[1].label == "✌️")
+        {
+            document.getElementById("update_emoji2").innerHTML="✌️ is Two";
+        }  
+        
+    }
+}
+{
+
+}
